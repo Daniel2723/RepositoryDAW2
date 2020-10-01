8aff49379b66256b54858a87f45a1a73d4b32080
daniel.ortega7 <71818234+danielortega7DAW@users.noreply.github.com>
2020-10-01 10:25:02 +0200

    Update ej2s.php

--- clases/ej2s.php
+++ clases/ej2s.php
@@ -4,40 +4,31 @@
     <?php
         $ip="25.1.255.255";
         $aux=$ip;
-        $ip=str_pad($ip,strlen($ip)+1,".");
     
-        $pos=stripos($ip, ".");
-        $ip1=substr($ip, 0, $pos);
+        $pos=stripos($ip, ".");     //Igual que en el anterior ejercicio, corto las
+        $ip1=substr($ip, 0, $pos);  //cadenas analizando la pos de los puntos
         $ip=substr($ip, $pos+1, strlen($ip));
     
         $pos=stripos($ip, ".");
         $ip2=substr($ip, 0, $pos);
         $ip=substr($ip, $pos+1, strlen($ip));
+    
         $pos=stripos($ip, ".");
         $ip3=substr($ip, 0, $pos);
         $ip=substr($ip, $pos+1, strlen($ip));
     
-        $pos=stripos($ip, ".");
-        $ip4=substr($ip, 0, $pos);
-        $ip=substr($ip, $pos+1, strlen($ip));
-    
-        $int1=intval($ip1);
-        $int2=intval($ip2);
-        $int3=intval($ip3);
-        $int4=intval($ip4);
-    
-        $bin1=decbin($ip1);
+        $bin1=decbin($ip1);     //Paso a binario usando "decbin"
         $bin2=decbin($ip2);
         $bin3=decbin($ip3);
-        $bin4=decbin($ip4);
+        $bin4=decbin($ip);
         
-        $bin1=str_pad($bin1, 8, "0", STR_PAD_LEFT);
-        $bin2=str_pad($bin2, 8, "0", STR_PAD_LEFT);
-        $bin3=str_pad($bin3, 8, "0", STR_PAD_LEFT);
+        $bin1=str_pad($bin1, 8, "0", STR_PAD_LEFT);   //Relleno con 0s hasta un maximo
+        $bin2=str_pad($bin2, 8, "0", STR_PAD_LEFT);   //de 8 posiciones, con "str_pad"  
+        $bin3=str_pad($bin3, 8, "0", STR_PAD_LEFT);   //y usando "STR_PAD_LEFT"  
         $bin4=str_pad($bin4, 8, "0", STR_PAD_LEFT);
     
         echo "La direccion ip $aux en binario es: ".$bin1.".".$bin2.".".$bin3.".".$bin4;
-        
+        //Escribo el resultado final añadiendo los puntos necesarios 
      ?>
 </BODY>
 </HTML>
